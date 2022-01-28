0614afa640c671f22163634c7b9d0c25acf13cf7
afif <afif@garasilabs.com>
2022-01-28 09:48:38 +0700

    feat: add composing types

---
+++ src/composingTypes.ts
@@ -0,0 +1,13 @@
+type myBool = true | false;
+type WindowStates = "open" | "closed" | "minimized";
+type LockStates = "locked" | "unlocked";
+type PositiveOddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+
+const myboolean: myBool = true;
+const windowstate: WindowStates = "minimized";
+const lockstate: LockStates = "locked";
+const positiveOddUnderTen: PositiveOddNumbersUnderTen = 3;
+console.log(myboolean);
+console.log(windowstate);
+console.log(lockstate);
+console.log(positiveOddUnderTen);
